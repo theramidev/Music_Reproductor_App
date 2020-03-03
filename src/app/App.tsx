586bb9523d5aa28679339d0005f5fa9f65ef3c15
Miguel Ramírez <miguelramidev@gmail.com>
2020-03-03 03:31:49 -0400

    Primera versión de producción

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-view';
 import Router from './Router';
+
+import SplashScreen from 'react-native-splash-screen';
 
 import { Provider, store } from './store';
 import { Layout } from './components/Layout';
 
 const App: FC<any> = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
