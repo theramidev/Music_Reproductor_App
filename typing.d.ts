6e123602b7a6be5da33d400bfd2515a7d84c9f0a
Miguel Ramírez <miguelramidev@gmail.com>
2020-03-14 17:12:42 -0400

    Se cambió el icono de la aplicación

--- typing.d.ts
+++ typing.d.ts
@@ -2,3 +2,4 @@ declare module 'react-native-get-music-files';
 declare module 'react-native-background-color';
 declare module 'react-native-material-ripple';
 declare module 'react-native-auto-scrolling';
+declare module '@react-native-community/checkbox';
