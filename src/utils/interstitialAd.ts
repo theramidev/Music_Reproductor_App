8a290c9c998756a6a05ad5bd3684a2e8203b61f0
Miguel Ramírez <miguelramidev@gmail.com>
2020-03-23 22:09:22 -0400

    Se cambió la versión de la aplicación

--- src/utils/interstitialAd.ts
+++ src/utils/interstitialAd.ts
@@ -1,6 +1,6 @@
 import { AdMobInterstitial } from 'react-native-admob';
 
-export const isPremium = false;
+export const isPremium = true;
 
 export const showAd = () => {
     AdMobInterstitial.setAdUnitID(__DEV__ ? 'EMULATOR' : 'ca-app-pub-2449976564245125/9262979175');
