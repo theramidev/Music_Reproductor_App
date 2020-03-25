3f51eeee1c1267a6dccb1a0691781adeb19af4a0
Miguel Ramírez <miguelramidev@gmail.com>
2020-03-25 18:43:27 -0400

    (fix) Ya no se duplican los archivos al compartir

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -2,7 +2,6 @@ import { MSong } from "../app/models/song.model";
 import Share, { Options } from 'react-native-share';
 import fs from 'react-native-fs';
 import AsyncStorage from '@react-native-community/async-storage';
-import i18n from 'i18next';
 import i18next from "i18next";
 
 /**
@@ -11,38 +10,40 @@ import i18next from "i18next";
  * @return Promise<void>
  */
 export default async (song: MSong): Promise<void> => {
-    const base64 = await fs.readFile(`file://${song.path}`, 'base64');
-    const songPathArray: string[] = song.path.split('.').reverse();
-    const filename: string = `${song.title}.${songPathArray[0]}`;
-    // console.log(filename);
+    // const base64 = await fs.readFile(`file://${song.path}`, 'base64');
+    // const songPathArray: string[] = song.path.split('.').reverse();
+    // const filename: string = `${song.title}`;
+    // console.log(song, filename);
     try {
       const options: Options = {
         failOnCancel: false,
-        title: i18next.t('share'), // Traducir esto
-        url: `data:audio/mp3;base64,${base64}`,
-        // url: 'file://'+song.path ,
-        filename,
-        type: 'audio/mp3',
-        showAppsToView: true
+        title: i18next.t('share'),
+        // url: `data:audio/mp3;base64,${base64}`,
+        url: 'file://'+song.path,
+        // filename,
+        type: 'audio/mp3'
       }
 
       const result = await Share.open(options);
+      // console.log(result);
 
-      if (!result.dismissedAction) {
-        const path = `file://${fs.DownloadDirectoryPath}/${filename}`;
-        const sharesStorage = await AsyncStorage.getItem('@shares');
-        if (sharesStorage) {
-          const sharesArray: string[] = JSON.parse(sharesStorage);
+      // if (!result.dismissedAction) {
+      //   const path = `file://${fs.DownloadDirectoryPath}/${filename}`;
+      //   const sharesStorage = await AsyncStorage.getItem('@shares');
+      //   if (sharesStorage) {
+      //     const sharesArray: string[] = JSON.parse(sharesStorage);
 
-          sharesArray.push(path);
-          await AsyncStorage.setItem('@shares', JSON.stringify(sharesArray));
-        } else {
-          const sharesArray = [];
-          sharesArray.push(path);
-          await AsyncStorage.setItem('@shares', JSON.stringify(sharesArray));
-        }
-      }
+      //     sharesArray.push(path);
+      //     await AsyncStorage.setItem('@shares', JSON.stringify(sharesArray));
+      //   } else {
+      //     const sharesArray = [];
+      //     sharesArray.push(path);
+      //     await AsyncStorage.setItem('@shares', JSON.stringify(sharesArray));
+      //   }
+      // }
     } catch (error) {
+      console.warn(error);
+      
       console.error('Share error: ', error);
     }
 }
