1e37de051fcf89b0f8ea5fb65fa711a94b4473e8
Miguel Ramírez <miguelramidev@gmail.com>
2020-03-27 23:19:44 -0400

    Se quitó el splash animado inicial

--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -21,48 +21,14 @@ import style from './style';
 import FooterMusic from '../../components/FooterMusic';
 
 class HomeScreen extends Component<IProps, IState> {
-  state = {
-    inSplash: true,
-    springVal: new Animated.Value(0.8),
-    fadeVal: new Animated.Value(1),
-    fadePrincipal: new Animated.Value(0)
-  };
   constructor(props: any) {
     super(props);
   }
 
   async componentDidMount() {
-    setTimeout(() => this.spring(), 500);
     this.props.getSongs();
     this.props.getCurrentWallpaper();
   }
-
-  spring = () => {
-    Animated.sequence([
-      Animated.spring(this.state.springVal, {
-        toValue: 0.6,
-        friction: 7,
-        tension: 20,
-      }),
-      Animated.parallel([
-        Animated.spring(this.state.springVal, {
-          toValue: 17.5,
-          friction: 7,
-          tension: 5,
-        }),
-        Animated.timing(this.state.fadeVal, {
-          toValue: 0,
-          duration: 300,
-        }),
-      ]),
-    ]).start(() => {
-      this.setState({inSplash: false});
-      Animated.timing(this.state.fadePrincipal, {
-        toValue: 1,
-        duration: 300,
-      }).start();
-    });
-  };
 
   render() {
     const {navigation, musicReducer} = this.props;
@@ -77,70 +43,48 @@ class HomeScreen extends Component<IProps, IState> {
     }
 
     return (
-      <>
-        {this.state.inSplash && (
-          <View style={style.wrapper}>
-            <View style={style.center}>
-              <Animated.View
-                style={{
-                  opacity: this.state.fadeVal,
-                  transform: [{scale: this.state.springVal}],
-                }}>
-                <Image
-                  source={require('../../../assets/images/splash.png')}
-                  style={style.splashImage}
-                />
-              </Animated.View>
-            </View>
-          </View>
-        )}
-        <Animated.View
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{flex: 1, height: '100%', opacity: this.state.fadePrincipal}}>
-          <BackgroundLayout>
-              {this.props.wallpaperReducer.data.currentWallpaper && (
-                <Image
-                  source={{
-                    uri: this.props.wallpaperReducer.data.currentWallpaper,
-                  }}
-                  style={[
-                    style.backgroundImage,
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    {
-                      height:
-                        Object.keys(this.props.musicReducer.current).length === 0
-                          ? '103%'
-                          : '95%',
-                    },
-                  ]}
-                />
-              )}
-
-              <Header navigate={navigation.navigate} />
-
-              <Sections navigation={this.props.navigation} />
-
-              <ListOfMusic
-                songs={listSongs}
-                updateFavorite={this.props.updateFavorite}
-                deleteSong={this.props.deleteSong}
-                navigate={navigation.navigate}
-                paddingBottom={
+      <BackgroundLayout>
+        {this.props.wallpaperReducer.data.currentWallpaper && (
+          <Image
+            source={{
+              uri: this.props.wallpaperReducer.data.currentWallpaper,
+            }}
+            style={[
+              style.backgroundImage,
+              // eslint-disable-next-line react-native/no-inline-styles
+              {
+                height:
                   Object.keys(this.props.musicReducer.current).length === 0
-                    ? 170
-                    : 230
-                }
-                refreshing={this.props.musicReducer.refreshing}
-                onRefresh={this.props.refreshListSong}
-              />
+                    ? '103%'
+                    : '95%',
+              },
+            ]}
+          />
+        )}
 
-              <FooterMusic
-                // @ts-ignore
-                navigation={navigation}
-              />
-          </BackgroundLayout>
-        </Animated.View>
-      </>
+        <Header navigate={navigation.navigate} />
+
+        <Sections navigation={this.props.navigation} />
+
+        <ListOfMusic
+          songs={listSongs}
+          updateFavorite={this.props.updateFavorite}
+          deleteSong={this.props.deleteSong}
+          navigate={navigation.navigate}
+          paddingBottom={
+            Object.keys(this.props.musicReducer.current).length === 0
+              ? 170
+              : 230
+          }
+          refreshing={this.props.musicReducer.refreshing}
+          onRefresh={this.props.refreshListSong}
+        />
+
+        <FooterMusic
+          // @ts-ignore
+          navigation={navigation}
+        />
+      </BackgroundLayout>
     );
   }
 }
