f5eeca829bb99b084764843d1743a2b1a35a88d6
David Eduardo Ramirez Soto <david.ramirez1448@gmail.com>
2020-03-31 17:40:56 -0400

    arreglado el problema de playlist

--- src/app/screens/Playlists/components/ListOfPlaylists/index.tsx
+++ src/app/screens/Playlists/components/ListOfPlaylists/index.tsx
@@ -1,35 +1,38 @@
-import React, { FC } from 'react';
-import { ScrollView, View } from 'react-native';
-import { IProps } from './PropsInterface';
+import React, {FC} from 'react';
+import {ScrollView, View} from 'react-native';
+import {IProps} from './PropsInterface';
 import styles from './style';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
-import { PlaylistCard } from '../PlaylistCard';
+import {PlaylistCard} from '../PlaylistCard';
 
-export const ListOfPlaylists: FC<IProps> = ({navigation, playlists, onCreate}) => {
-    const { t } = useTranslation('ListOfPlaylists');
+export const ListOfPlaylists: FC<IProps> = ({
+  navigation,
+  playlists,
+  onCreate,
+}) => {
+  const {t} = useTranslation('ListOfPlaylists');
 
-    return(
-        <ScrollView>
-            <View style={styles.container}>
-                <PlaylistCard 
-                    mode="add" 
-                    title={t('createList')}
-                    onCreate={onCreate} 
-                    navigation={navigation} 
-                />
-                {
-                    playlists.map((playlist, i) => {
-                        return(
-                            <PlaylistCard key={i}
-                                mode="playlist" 
-                                playlist={playlist}
-                                navigation={navigation}
-                            />
-                        )
-                    })
-                }
-            </View>
-        </ScrollView>
-    )
-}
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <PlaylistCard
+          mode="add"
+          title={t('createList')}
+          onCreate={onCreate}
+          navigation={navigation}
+        />
+        {playlists.map((playlist, i) => {
+          return (
+            <PlaylistCard
+              key={i}
+              mode="playlist"
+              playlist={playlist}
+              navigation={navigation}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
+  );
+};
